3af816435f967a363ef32bdd45914e10fd83b17f
Pavel <pawelch@yandex.ru>
2018-08-26 15:19:58 +0400

    Новая версия

---
+++ random/random/Generate.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Павел
+ * Date: 26.08.2018
+ * Time: 15:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace random
+{
+	/// <summary>
+	/// Description of Generate.
+	/// </summary>
+	public class Generate
+	{
+		public Generate()
+		{
+		}
+	}
+}
